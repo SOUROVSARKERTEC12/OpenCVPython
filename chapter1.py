9d17dceaa869ce861d81806475c1b992b0b4b3a2
Sourov <S@gmail.com>
2021-09-07 01:54:32 +0600

    Image Gray

--- chapter1.py
+++ chapter1.py
@@ -1,13 +1,9 @@
 import cv2
 print("Package Imported")
 
-cap = cv2.VideoCapture(1)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
+img = cv2.imread("Resources/Devil.jpg")
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.waitKey(0)
