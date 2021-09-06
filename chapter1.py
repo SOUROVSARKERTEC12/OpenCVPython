8df3916e0ff7c73d4df7932907777a6e9b82e95b
Sourov <S@gmail.com>
2021-09-07 02:14:52 +0600

    Cropped Image

--- chapter1.py
+++ chapter1.py
@@ -4,8 +4,12 @@ print("Package Imported")
 
 img = cv2.imread("Resources/Devil.jpg")
 print(img.shape)
-imgResize = cv2.resize(img, (400, 400))
+imgResize = cv2.resize(img, (400, 400)) # 1st width and 2nd height
+print(imgResize.shape)
+
+imgCropped = img[0:200, 200:500] # 1st height and 2nd width
 
 cv2.imshow("Image", img)
 cv2.imshow("Resize Image", imgResize)
+cv2.imshow("Cropped Image", imgCropped)
 cv2.waitKey(0)
