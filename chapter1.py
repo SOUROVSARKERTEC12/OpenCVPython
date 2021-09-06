ec1cb94e63cd323bbaaf36e34950631f7fcf7843
Sourov <S@gmail.com>
2021-09-07 01:57:31 +0600

    Blur Gray

--- chapter1.py
+++ chapter1.py
@@ -4,6 +4,8 @@ print("Package Imported")
 img = cv2.imread("Resources/Devil.jpg")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
 
 cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
 cv2.waitKey(0)
