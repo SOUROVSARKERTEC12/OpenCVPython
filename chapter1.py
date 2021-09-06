175a5bb67fd4b02ed6acc189857665b20a0ab9dd
Sourov <S@gmail.com>
2021-09-07 01:59:39 +0600

    Canny image

--- chapter1.py
+++ chapter1.py
@@ -5,7 +5,9 @@ img = cv2.imread("Resources/Devil.jpg")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
+imgCanny = cv2.Canny(img, 100, 100)
 
 cv2.imshow("Gray Image", imgGray)
 cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image", imgCanny)
 cv2.waitKey(0)
